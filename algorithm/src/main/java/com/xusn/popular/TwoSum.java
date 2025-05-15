b3c3f17eea5a88e470aeb4f5ba9988b3caf4ce7e
xusn <839576673@qq.com>
2025-05-16 01:02:10 +0800

    两数之和

---
+++ algorithm/src/main/java/com/xusn/popular/TwoSum.java
@@ -0,0 +1,21 @@
+package com.xusn.popular;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 两数之和
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int j = target - nums[i];
+            if (map.containsKey(j)) {
+                return new int[] {map.get(j), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
