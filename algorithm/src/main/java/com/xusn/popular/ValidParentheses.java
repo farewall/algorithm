3b3b3b8420724e831daf7600012579cf4d0d8736
xusn <xusn@shterm.com>
2025-05-19 14:27:03 +0800

    有效的括号

---
+++ algorithm/src/main/java/com/xusn/popular/ValidParentheses.java
@@ -0,0 +1,42 @@
+package com.xusn.popular;
+
+
+import java.util.*;
+
+/**
+ * 合法的括号
+ */
+public class ValidParentheses {
+    public static boolean isValid(String input) {
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> map  = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        for (char c : input.toCharArray()) {
+            // 代表当前放的是左括号，直接放
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+               // 右括号需要检查栈顶的元素和当前的当前是否成对
+                Character peek = stack.peek();
+                // 放右括号，当前栈中没有元素
+                if (peek == null) {
+                    return false;
+                } else {
+                    // 当前栈中有元素
+                    if (map.get(peek) != c) {
+                        return false;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+    }
+}
