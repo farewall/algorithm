390605952f85200a255f76a4b028942fef3fbe53
xusn <xusn@shterm.com>
2025-07-16 14:12:51 +0800

    合并区间 & 相交链表

---
+++ algorithm/src/main/java/com/xusn/second/IntersectionNode.java
@@ -0,0 +1,38 @@
+package com.xusn.second;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 相交链表
+ *
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ *
+ * 图示两个链表在节点 c1 开始相交：
+ */
+public class IntersectionNode {
+    public class ListNode {
+        int val;
+         ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
