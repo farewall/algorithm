e03a8fc4ee804678dc844e6f810bf66b97d6ba39
xusn <xusn@shterm.com>
2025-07-25 16:12:49 +0800

    爬楼梯 & 零钱兑换 （动态规划）

---
+++ algorithm/src/main/java/com/xusn/third/CoinChange.java
@@ -0,0 +1,28 @@
+package com.xusn.third;
+
+import java.util.Arrays;
+
+/**
+ * 零钱兑换
+ *
+ * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+ *
+ * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+ *
+ * 你可以认为每种硬币的数量是无限的。
+ */
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i >= coin) {
+                    dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
