835215a1918e4daba0d757630c76360a67fcf5c3
xusn <xusn@shterm.com>
2025-08-04 17:05:47 +0800

    排序数组中指定数字的区间（二分）

---
+++ algorithm/src/main/java/com/xusn/fourth/SearchRange.java
@@ -0,0 +1,45 @@
+package com.xusn.fourth;
+
+public class SearchRange {
+    public int[] searchRange(int[] nums, int target) {
+        int first = binarySearchFirst(nums, target);
+        int last = binarySearchLast(nums, target);
+        return new int[] {first, last};
+    }
+
+    public int binarySearchFirst(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int res = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                res = mid;
+                right = mid - 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    public int binarySearchLast(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int res = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                res = mid;
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}
