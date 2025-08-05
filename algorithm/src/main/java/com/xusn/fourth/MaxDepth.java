28829923b6d5c0dde05ceb4e32efca6eeb6975dd
xusn <xusn@shterm.com>
2025-08-05 17:15:54 +0800

    二叉树最大深度 & 组合总和

---
+++ algorithm/src/main/java/com/xusn/fourth/MaxDepth.java
@@ -0,0 +1,26 @@
+package com.xusn.fourth;
+
+/**
+ * 二叉树的最大深度
+ */
+public class MaxDepth {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+  public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+  }
+}
