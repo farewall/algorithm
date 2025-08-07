caf35e20c3a5bf80ae3aeab46bf0f581ea2fb1fa
xusn <xusn@shterm.com>
2025-08-07 15:29:41 +0800

    验证二叉搜索树 & 二维矩阵搜索

---
+++ algorithm/src/main/java/com/xusn/fourth/SearchMatrix.java
@@ -0,0 +1,31 @@
+package com.xusn.fourth;
+
+/**
+ * 搜索二维矩阵2
+ *
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ *
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ */
+public class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int m = matrix.length;;
+        int n = matrix[0].length;
+        int row = 0;
+        int col = n-1;
+        while (row < m && col >=0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
